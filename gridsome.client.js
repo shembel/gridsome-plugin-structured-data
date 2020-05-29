fe695e98cf825e11f14a27a9655c4b5ca468f78c
colus-img <colus.img@gmail.com>
2020-05-29 17:29:40 +0900

    fix: remove duplicate tags

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,10 @@
 import sd from './lib/sd';
 
 export default function (Vue, options, context) {
-	Vue.prototype.$sd = (finalOptions) => {
+	Vue.prototype.$sd = (pageOptions) => {
 		//console.log(context);
 		//console.log(sd(options, finalOptions, context));
-		return sd(options, finalOptions, context);
+		//console.log(this);
+		return sd(options, pageOptions, context);
 	}
 }
